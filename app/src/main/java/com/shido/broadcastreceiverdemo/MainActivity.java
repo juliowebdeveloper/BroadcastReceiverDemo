18ab946a9111ef3ea38a036c5f1993e396565b0f
Julio <juliowebdeveloper@hotmail.com>
2016-12-28 11:31:12 -0200

    OrderedBroadcasts

--- app/src/main/java/com/shido/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/shido/broadcastreceiverdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shido.broadcastreceiverdemo;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,9 +28,55 @@ public class MainActivity extends AppCompatActivity {
        // Intent i = new Intent(this, MyFirstReceiver.class);
         //sendBroadcast(i);
         Intent intent = new Intent("my.custom.action.name"); //Passando a action como parametro da intent
-        intent.putExtra("name", "Shido");
-        intent.putExtra("age", 27);
-        sendBroadcast(intent);
+
+       /*intent.putExtra("name", "Shido");
+        intent.putExtra("age", 27);*/
+
+
+        Bundle b = new Bundle();
+        b.putInt("age", 27);
+        b.putString("name", "Shido");
+
+
+        /*void sendOrderedBroadcast (Intent intent,
+                String receiverPermission,
+                BroadcastReceiver resultReceiver,
+                Handler scheduler,
+        int initialCode,
+        String initialData,
+        Bundle initialExtras)
+
+        Parameters
+                intent
+        Intent: The Intent to broadcast; all receivers matching this Intent will receive the broadcast.
+                receiverPermission
+        String: String naming a permissions that a receiver must hold in order to receive your broadcast. If null, no permission is required.
+                resultReceiver
+        BroadcastReceiver: Your own BroadcastReceiver to treat as the final receiver of the broadcast.
+        scheduler
+        Handler: A custom Handler with which to schedule the resultReceiver callback; if null it will be scheduled in the Context's main thread.
+        initialCode
+        int: An initial value for the result code. Often Activity.RESULT_OK.
+                initialData
+        String: An initial value for the result data. Often null.
+                initialExtras
+        Bundle: An initial value for the result extras. Often null.*/
+
+
+        sendOrderedBroadcast(intent, null, null, null, Activity.RESULT_OK, "Android",b);
+
+
+
+        // Enviando um Broadcast que verificar a prioridade dos intent filters quanto maior a prioridade primeiro ele vem
+        //Mesma action:name
+
+      /*
+        Com isso a informação será passada para os BroadcastReceivers em ordem de prioridade
+            cada um pode pegar essa informação e modifica-la e passa-la adianta
+            caso deseje terminar a transmissao usa-se abortBroadcast();
+
+            */
+
         //Toast para mostrar que os Broadcast Receiver são executados asynchronous
 
         Toast.makeText(MainActivity.this, "Toast From Main Activity", Toast.LENGTH_SHORT).show();
@@ -39,10 +86,10 @@ public class MainActivity extends AppCompatActivity {
        // Intent i = new Intent(this, MyThirdReceiverInner.class);
         //sendBroadcast(i);
         Intent intent = new Intent("my.custom.anotheraction.name");
-        Bundle bundle = new Bundle();
+       /* Bundle bundle = new Bundle();
         bundle.putInt("age1", 33);
         bundle.putString("name1", "Shido");
-        intent.putExtras(bundle);
+        intent.putExtras(bundle);*/
         sendBroadcast(intent);
 
 
@@ -53,10 +100,10 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            String name = intent.getStringExtra("name1");
+           /* String name = intent.getStringExtra("name1");
             int age = intent.getIntExtra("age1", 0);
+             Log.i(TAG, "NAME IS: " + name +" and age is " + String.valueOf(age)); */
 
-            Log.i(TAG, "NAME IS: " + name +" and age is " + String.valueOf(age));
                 Log.i(TAG, "hello from third Receiver");
             Toast.makeText(context, "Hello from third receiver", Toast.LENGTH_LONG).show();
 
