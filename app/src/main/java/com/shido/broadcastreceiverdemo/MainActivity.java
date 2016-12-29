425e30de179f8e0d070b332ebab9512c9ece86dd
Julio <juliowebdeveloper@hotmail.com>
2016-12-29 09:34:04 -0200

    OrderedBroadcasts

--- app/src/main/java/com/shido/broadcastreceiverdemo/MainActivity.java
+++ app/src/main/java/com/shido/broadcastreceiverdemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,20 +16,30 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+
+    private MyFirstReceiver myFirstReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myFirstReceiver = new MyFirstReceiver();
     }
 
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
-
-    public void sendBroadCastMessage(View view) {
-
-
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        registerReceiver(myFirstReceiver, intentFilter);
     }
 
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(myFirstReceiver);
 
     }
+}
